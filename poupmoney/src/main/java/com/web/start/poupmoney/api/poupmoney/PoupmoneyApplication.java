ff53d606312d323636f303643f5a256402074c14
Geneton Neto <geneton.alves.neto@gmail.com>
2023-07-30 05:12:23 -0300

    adicionando repo poupmoney ao github

---
+++ poupmoney/src/main/java/com/web/start/poupmoney/api/poupmoney/PoupmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.web.start.poupmoney.api.poupmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoupmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoupmoneyApplication.class, args);
+	}
+
+}
