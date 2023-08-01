b204f4bd4c778dbddbe23a216de7e282092b34a0
Gabriel <gabrielldornelas@hotmail.com>
2023-08-01 18:10:16 -0300

    Debugging UserSpringSecurity

--- poupmoney/src/main/java/com/web/start/poupmoney/api/poupmoney/security/UserSpringSecurity.java
+++ poupmoney/src/main/java/com/web/start/poupmoney/api/poupmoney/security/UserSpringSecurity.java
@@ -2,6 +2,7 @@ package com.web.start.poupmoney.api.poupmoney.security;
 
 import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -23,54 +24,36 @@ public class UserSpringSecurity implements UserDetails {
     private Collection<? extends GrantedAuthority> authorities;
      
     public UserSpringSecurity(Long id, String username, String password, Double budget,
-            Set<ProfileEnum> profileEnumsrofileEnums) {
+            Set<ProfileEnum> profileEnums) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.budget = budget;
-        this.authorities = profileEnums.
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
-    }
-
-    @Override
-    public String getPassword() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPassword'");
-    }
-
-    @Override
-    public String getUsername() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
+        this.authorities = profileEnums.stream().map(x -> new SimpleGrantedAuthority(x.getDescription())).collect(Collectors.toList());
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isAccountNonExpired'");
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isAccountNonLocked'");
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isCredentialsNonExpired'");
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isEnabled'");
+        return true;
+    }
+
+    public boolean hasRole(ProfileEnum profileEnum){
+        return getAuthorities().contains(new SimpleGrantedAuthority(profileEnum.getDescription()));
     }
     
 }
